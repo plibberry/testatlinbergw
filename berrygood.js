7f89f0101059ebfd9cf4cf9028297c157c9c263d
plibberry <plibaryberrygoodsigs@gmail.com>
2025-06-30 19:40:20 -0700

    Create berrygood.js

---
+++ berrygood.js
@@ -0,0 +1,20 @@
+const problem1 = {
+  question: 'Differentiate: f(x) = 3x^4 - 5x^2 + 2x - 7',
+  answer: '12x^3 - 10x + 2'
+};
+const problem2 = {
+  question: 'Differentiate: k(x) = sin(3x^2)',
+  answer: 'cos(3x^2) * 6x'
+};
+const problem3 = {
+  question: 'Differentiate: h(x)=5x^3-4x^2+2x-32',
+  answer: '15x^20-20x^4+2x'
+};
+const problem4 = {
+  question: 'Differentiate: g(x)=(x^2+1)/(x^3-2x)',
+  answer: '(-x^4-5x^2+2)/(x^3-2x)^2x'
+};
+const problem5={
+  questions: 'Differentiate: p(x)=tan(5x^2)',
+  answer: '10x*sec^2(5x^2)'
+};
